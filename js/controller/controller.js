f18d774fdddee5e2398322a0825fce76fc5c4c3d
Emanuel Ben Hamo <embenh@gmail.com>
2020-02-12 12:59:48 +0200

    added text line

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,1 +1,47 @@
- 'use strict'
+'use strict'
+
+let gElCanvas;
+let gCtx;
+
+function init() {
+    gElCanvas = document.getElementById('canvas');
+    gCtx = gElCanvas.getContext('2d');
+    renderMeme();
+}
+
+function renderMeme() {
+    let bgImg = new Image();
+    bgImg.src = getBgImgUrl();
+    bgImg.onload = () => {
+        gCtx.drawImage(bgImg, 0, 0, gElCanvas.width, gElCanvas.height);
+        drawTextLines();
+    }
+}
+
+function drawTextLines() {
+    let txtLines = getAllTxtLines();
+    txtLines.forEach((txtLine, txtLineIdx) => drawTextLine(txtLine, txtLineIdx));
+}
+
+function drawTextLine(textLine, textLineIndex) {
+    let fontFamily = "Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif";
+    gCtx.font = `${textLine.size}px ${fontFamily}`;
+    gCtx.textAlign = textLine.align;
+    gCtx.fillStyle = textLine.color;
+    gCtx.strokeStyle = 'black';
+    let textLineHeight;
+
+    if (textLineIndex === 0) {
+        textLineHeight = 0;
+        gCtx.textBaseline = 'top';
+    } else if (textLineIndex === 1) {
+        textLineHeight = gElCanvas.height;
+        gCtx.textBaseline = 'bottom';
+    } else {
+        textLineHeight = gElCanvas.height / 2;
+        gCtx.textBaseline = 'middle';
+    }
+
+    gCtx.fillText(textLine.txt, gElCanvas.width / 2, textLineHeight);
+    gCtx.strokeText(textLine.txt, gElCanvas.width / 2, textLineHeight);
+}
--- js/service/memeService.js
+++ js/service/memeService.js
@@ -1,16 +1,37 @@
 'use strict'
 
 let gMeme = {
-    selectedImgId: null,
-    selectedLineIdx: null,
+    selectedImgId: 2,
+    selectedLineIdx: 0,
     lines: [
         {
-            txt: null,
-            size: null,
-            align: null,
-            color: null
+            txt: 'first text',
+            size: 50,
+            align: 'center',
+            color: 'red'
         }
     ]
+}
+
+function getMeme() {
+    return gMeme;
+}
+
+function getBgImgUrl() {
+    let imgUrl = `/images/square/${gMeme.selectedImgId}.jpg`;
+    return imgUrl;
+}
+
+function getAllTxtLines() {
+    return gMeme.lines;
+}
+
+function getSelctedTxtLine() {
+    return gMeme.lines[gMeme.selectedLineIdx];
+}
+
+function getSelectedLineIdx() {
+    return gMeme.selectedLineIdx;
 }
 
 
