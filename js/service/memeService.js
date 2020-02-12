d1f66eb9afd67f3c21ec6d6cbbfc1e172cd235b1
Emanuel Ben Hamo <embenh@gmail.com>
2020-02-12 11:20:08 +0200

    created initail gMeme

---
+++ js/service/memeService.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let gMeme = {
+    selectedImgId: null,
+    selectedLineIdx: null,
+    lines: [
+        {
+            txt: null,
+            size: null,
+            align: null,
+            color: null
+        }
+    ]
+}
+
+
+
